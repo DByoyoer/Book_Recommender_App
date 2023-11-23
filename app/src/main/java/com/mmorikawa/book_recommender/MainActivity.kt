66b481d8bb61a619499eced00098f35bd0e87f2c
Michael Morikawa <michael.f.morikawa@gmail.com>
2023-11-22 23:08:53 -0800

    Initial Project Setup

---
+++ app/src/main/java/com/mmorikawa/book_recommender/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.mmorikawa.book_recommender
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mmorikawa.book_recommender.ui.theme.BookRecommenderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BookRecommenderTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BookRecommenderTheme {
+        Greeting("Android")
+    }
+}
