f61ae9488aa3e5173f24696b935e8948b999fac4
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-02-17 22:31:14 -0800

    Convert :core:testing to new build system

--- core/testing/build.gradle.kts
+++ core/testing/build.gradle.kts
@@ -1,35 +1,11 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("book_recommender.android.library")
 }
 
 android {
-    namespace = "com.mmorikawa.core.testing"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    namespace = "com.mmorikawa.book_recommender.core.testing"
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
     implementation(project(":core:model"))
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
