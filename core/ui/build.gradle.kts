fff05708dbf634bde1d8b5d8d7026ab9585ad526
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-02-17 22:38:42 -0800

    Convert :core:ui to new build system

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -1,49 +1,22 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("book_recommender.android.library")
+    id("book_recommender.android.library.compose")
 }
 
 android {
-    namespace = "com.mmorikawa.core.ui"
-    compileSdk = 34
+    namespace = "com.mmorikawa.book_recommender.core.ui"
 
     defaultConfig {
-        minSdk = 28
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-
-    buildFeatures {
-        compose = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
 }
 
 dependencies {
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.material3)
 
     implementation(project(":core:model"))
-    val composeBom = platform("androidx.compose:compose-bom:2024.01.00")
-    androidTestImplementation(composeBom)
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.compose.foundation:foundation-android")
-    implementation("androidx.compose.material3:material3")
     implementation(project(":core:designsystem"))
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
+    
 }
