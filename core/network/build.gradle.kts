0b86adb894b78ee5c4b6d0879faec89c6f03e9c0
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-04-07 18:49:46 -0700

    Add dependencies for core:network module

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,27 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("book_recommender.android.library")
+    id("book_recommender.android.hilt")
+    id("kotlinx-serialization")
+}
+
+android {
+    namespace = "com.mmorikawa.book_recommender.core.database"
+
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:model"))
+
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.client.auth)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    androidTestImplementation(":core:testing")
+}
