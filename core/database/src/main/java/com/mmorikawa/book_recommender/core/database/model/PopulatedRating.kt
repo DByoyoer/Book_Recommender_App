bb34f1cdeba30c350647d4f11a9644a8b0d659f6
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-04-23 14:05:59 -0700

    Add missing annotations for PopulatedRating and PopulatedReadingListEntity

--- core/database/src/main/java/com/mmorikawa/book_recommender/core/database/model/PopulatedRating.kt
+++ core/database/src/main/java/com/mmorikawa/book_recommender/core/database/model/PopulatedRating.kt
@@ -1,13 +1,19 @@
 package com.mmorikawa.book_recommender.core.database.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import com.mmorikawa.core.model.Rating
 import kotlinx.datetime.Instant
 
 data class PopulatedRating(
+    @Embedded
     val book: PopulatedBasicBook,
+    @ColumnInfo(name = "date_created")
     val dateCreated: Instant,
+    @ColumnInfo(name = "date_updated")
     val dateUpdated: Instant,
     val score: Int,
+    @ColumnInfo(name = "rating_text")
     val ratingText: String,
 )
 
--- core/database/src/main/java/com/mmorikawa/book_recommender/core/database/model/PopulatedReadingListEntity.kt
+++ core/database/src/main/java/com/mmorikawa/book_recommender/core/database/model/PopulatedReadingListEntity.kt
@@ -1,10 +1,14 @@
 package com.mmorikawa.book_recommender.core.database.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import com.mmorikawa.core.model.ReadingListEntry
 import kotlinx.datetime.Instant
 
 data class PopulatedReadingListEntity(
+    @Embedded
     val book: PopulatedBasicBook,
+    @ColumnInfo(name = "date_added")
     val dateAdded: Instant,
     val ranking: Int,
 )
@@ -13,4 +17,10 @@ fun PopulatedReadingListEntity.asExternalModel() = ReadingListEntry(
     book = book.asExternalModel(),
     dateAdded = dateAdded,
     ranking = ranking
+)
+
+fun ReadingListEntry.asEntity(): ReadingListEntity = ReadingListEntity(
+    bookId = book.id,
+    ranking = ranking,
+    dateAdded = dateAdded
 )
