fbd9fcb25767cf42b36ec9dae949fa405550bd78
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-02 10:21:41 -0700

    Create binding for network data source

--- core/network/src/main/java/com/mmorikawa/book_recommender/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/mmorikawa/book_recommender/core/network/di/NetworkModule.kt
@@ -1,4 +1,18 @@
 package di
 
-object NetworkModule {
+import com.mmorikawa.book_recommender.core.network.BookRecNetworkDataSource
+import com.mmorikawa.book_recommender.core.network.ktor.KtorBookRecApiClient
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class NetworkModule {
+
+    @Singleton
+    @Binds
+    abstract fun bindsNetworkDataSource(ktorBookRecApiClient: KtorBookRecApiClient): BookRecNetworkDataSource
 }
