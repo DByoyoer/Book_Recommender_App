b53099dc5aff7d77791e57c97bde56d196f735eb
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-03 14:26:15 -0700

    Fix default request url

--- core/network/src/main/java/com/mmorikawa/book_recommender/core/network/ktor/KtorBookRecApiClient.kt
+++ core/network/src/main/java/com/mmorikawa/book_recommender/core/network/ktor/KtorBookRecApiClient.kt
@@ -41,7 +41,7 @@ class KtorBookRecApiClient @Inject constructor() : BookRecNetworkDataSource {
             level = LogLevel.ALL
         }
         defaultRequest {
-            url("10.0.2.2:8000/")
+            url("http://10.0.2.2:8000/")
         }
     }
 
