4c5ce424536f6d8b79d758268c6a1bc9c3d1dcad
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-09 16:26:37 -0700

    Make OfflineFirstReadingListRepository use ioDispatcher for its operations

--- core/data/src/main/java/com/mmorikawa/book_recommender/core/data/repository/OfflineFirstReadingListRepository.kt
+++ core/data/src/main/java/com/mmorikawa/book_recommender/core/data/repository/OfflineFirstReadingListRepository.kt
@@ -1,27 +1,35 @@
 package com.mmorikawa.book_recommender.core.data.repository
 
+import com.mmorikawa.book_recommender.core.common.dispatchers.BookRecDispatchers
+import com.mmorikawa.book_recommender.core.common.dispatchers.Dispatcher
 import com.mmorikawa.book_recommender.core.database.dao.ReadingListDao
 import com.mmorikawa.book_recommender.core.database.model.asEntity
 import com.mmorikawa.book_recommender.core.database.model.asExternalModel
 import com.mmorikawa.book_recommender.core.network.BookRecNetworkDataSource
 import com.mmorikawa.core.model.ReadingListEntry
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class OfflineFirstReadingListRepository @Inject constructor(
     private val network: BookRecNetworkDataSource,
-    private val readingListDao: ReadingListDao
+    private val readingListDao: ReadingListDao,
+    @Dispatcher(BookRecDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
 ) : ReadingListRepository {
     override suspend fun getReadingList(orderByDateAdded: Boolean): List<ReadingListEntry> =
-        readingListDao.getReadingListBooks(orderByDateAdded = false).map { it.asExternalModel() }
+        withContext(ioDispatcher) {
+            readingListDao.getReadingListBooks(orderByDateAdded = false)
+                .map { it.asExternalModel() }
+        }
 
     override suspend fun addReadingListEntry(readingListEntry: ReadingListEntry) =
-        readingListDao.upsertReadingListEntry(readingListEntry.asEntity())
+        withContext(ioDispatcher) { readingListDao.upsertReadingListEntry(readingListEntry.asEntity()) }
 
     override suspend fun updateReadingListEntryRanking(readingListEntry: ReadingListEntry) =
-        readingListDao.upsertReadingListEntry(readingListEntry.asEntity())
+        withContext(ioDispatcher) { readingListDao.upsertReadingListEntry(readingListEntry.asEntity()) }
 
     override suspend fun deleteReadingListEntry(bookIds: List<Int>) =
-        readingListDao.deleteReadingListEntriesByIds(bookIds)
+        withContext(ioDispatcher) { readingListDao.deleteReadingListEntriesByIds(bookIds) }
 }
 
 
