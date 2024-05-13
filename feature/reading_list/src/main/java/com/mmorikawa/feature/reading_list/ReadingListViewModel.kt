329b45564fd9409cee83ac8571ec67949d89a788
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-13 01:01:34 -0700

    Implement some features of the ReadingListViewModel

--- feature/reading_list/src/main/java/com/mmorikawa/feature/reading_list/ReadingListViewModel.kt
+++ feature/reading_list/src/main/java/com/mmorikawa/feature/reading_list/ReadingListViewModel.kt
@@ -1,9 +1,18 @@
 package com.mmorikawa.feature.reading_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mmorikawa.book_recommender.core.data.repository.ReadingListRepository
+import com.mmorikawa.core.model.ReadingListEntry
 import com.mmorikawa.core.ui.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,6 +20,45 @@ class ReadingListViewModel @Inject constructor(
     val readingListRepository: ReadingListRepository
 ) : ViewModel() {
     // TODO: Set up repository
-    var uiState = UiState.Loading
+    val _sortState = MutableStateFlow<Pair<SortValue, SortOrder>>(
+        Pair(
+            SortValue.DATE_ADDED,
+            SortOrder.ASCENDING
+        )
+    )
+    val uiState: StateFlow<UiState<List<ReadingListEntry>>> =
+        _sortState.flatMapLatest { sortOptions ->
+            readingListRepository.getReadingListStream(
+                orderByDateAdded = sortOptions.first == SortValue.DATE_ADDED,
+                ascending = sortOptions.second == SortOrder.ASCENDING
+            ).map { UiState.Success(it) }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = UiState.Loading
+        )
 
+    fun toggleOrder() {
+        _sortState.update {
+            Pair(
+                it.first,
+                if (it.second == SortOrder.ASCENDING) SortOrder.DESCENDING else SortOrder.ASCENDING
+            )
+        }
+    }
+
+    fun updateSortValue(value: SortValue) {
+        _sortState.update { Pair(value, it.second) }
+    }
+
+
+}
+
+
+enum class SortOrder {
+    ASCENDING, DESCENDING
+}
+
+enum class SortValue {
+    RANKING, DATE_ADDED
 }
