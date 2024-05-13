8124b1282ddf0ea97b59ed51b7d4362a9ccfcfbd
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-13 13:27:00 -0700

    Change function name for flow return and fix not using PopulatedRating as the return type

--- core/database/src/main/java/com/mmorikawa/book_recommender/core/database/dao/RatingDao.kt
+++ core/database/src/main/java/com/mmorikawa/book_recommender/core/database/dao/RatingDao.kt
@@ -2,9 +2,12 @@ package com.mmorikawa.book_recommender.core.database.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Upsert
+import com.mmorikawa.book_recommender.core.database.model.PopulatedRating
 import com.mmorikawa.book_recommender.core.database.model.RatingEntity
 import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Instant
 
 @Dao
 interface RatingDao {
@@ -13,8 +16,13 @@ interface RatingDao {
     suspend fun upsertRating(rating: RatingEntity)
 
     // TODO: Add order options
+    @Transaction
     @Query("SELECT * FROM rating ORDER BY date_updated DESC")
-    fun getRatings(): Flow<RatingEntity>
+    suspend fun getRatings(): List<PopulatedRating>
+
+    @Transaction
+    @Query("SELECT * FROM rating WHERE rating.date_updated >= :timeCutoff ORDER BY date_updated DESC")
+    fun observeAllRatings(timeCutoff: Instant): Flow<List<PopulatedRating>>
 
     @Query("DELETE FROM rating WHERE book_id in (:bookIds)")
     fun deleteRatingsByIds(bookIds: List<Int>)
