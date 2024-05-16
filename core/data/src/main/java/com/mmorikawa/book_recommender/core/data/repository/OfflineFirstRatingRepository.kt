ede944bdeb4741a2c3383e748e56c848bcf9af39
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-15 18:37:54 -0700

    Add getRatingsStream function to RatingRepository and implement the repository

--- core/data/src/main/java/com/mmorikawa/book_recommender/core/data/repository/OfflineFirstRatingRepository.kt
+++ core/data/src/main/java/com/mmorikawa/book_recommender/core/data/repository/OfflineFirstRatingRepository.kt
@@ -1,21 +1,47 @@
 package com.mmorikawa.book_recommender.core.data.repository
 
+import com.mmorikawa.book_recommender.core.common.dispatchers.BookRecDispatchers
+import com.mmorikawa.book_recommender.core.common.dispatchers.Dispatcher
+import com.mmorikawa.book_recommender.core.data.model.asNetworkModel
+import com.mmorikawa.book_recommender.core.database.dao.RatingDao
+import com.mmorikawa.book_recommender.core.database.model.PopulatedRating
+import com.mmorikawa.book_recommender.core.database.model.asEntity
+import com.mmorikawa.book_recommender.core.database.model.asExternalModel
+import com.mmorikawa.book_recommender.core.network.BookRecNetworkDataSource
 import com.mmorikawa.core.model.Rating
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import kotlinx.datetime.Instant
+import javax.inject.Inject
 
-class OfflineFirstRatingRepository : RatingRepository {
-    override suspend fun getRatings(): List<Rating> {
-        TODO("Not yet implemented")
+class OfflineFirstRatingRepository @Inject constructor(
+    private val ratingDao: RatingDao,
+    private val networkDataSource: BookRecNetworkDataSource,
+    @Dispatcher(BookRecDispatchers.IO) private val ioDispatcher: CoroutineDispatcher
+) : RatingRepository {
+    override suspend fun getRatings(): List<Rating> = withContext(ioDispatcher) {
+        ratingDao.getRatings().map(PopulatedRating::asExternalModel)
     }
 
-    override suspend fun createRating(rating: Rating) {
-        TODO("Not yet implemented")
+    override fun getRatingStream(timeCutoff: Instant): Flow<List<Rating>> =
+        ratingDao.observeAllRatings(timeCutoff).map { it.map(PopulatedRating::asExternalModel) }
+
+    override suspend fun createRating(rating: Rating) = withContext(ioDispatcher) {
+        ratingDao.upsertRating(rating.asEntity())
+        networkDataSource.createRating(rating.asNetworkModel())
     }
 
-    override suspend fun updateRating(rating: Rating) {
-        TODO("Not yet implemented")
+    override suspend fun updateRating(rating: Rating) = withContext(ioDispatcher) {
+        ratingDao.upsertRating(rating.asEntity())
+        networkDataSource.updateRating(rating.asNetworkModel())
     }
 
-    override suspend fun deleteRating(rating: Rating) {
-        TODO("Not yet implemented")
+    override suspend fun deleteRating(ids: List<Int>) = withContext(ioDispatcher) {
+        ratingDao.deleteRatingsByIds(ids)
+        for (id in ids) {
+            networkDataSource.deleteRating(id)
+        }
     }
 }
--- core/data/src/main/java/com/mmorikawa/book_recommender/core/data/repository/RatingRepository.kt
+++ core/data/src/main/java/com/mmorikawa/book_recommender/core/data/repository/RatingRepository.kt
@@ -1,13 +1,17 @@
 package com.mmorikawa.book_recommender.core.data.repository
 
 import com.mmorikawa.core.model.Rating
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Instant
 
 interface RatingRepository {
     suspend fun getRatings(): List<Rating>
+
+    fun getRatingStream(timeCutoff: Instant): Flow<List<Rating>>
 
     suspend fun createRating(rating: Rating)
 
     suspend fun updateRating(rating: Rating)
 
-    suspend fun deleteRating(rating: Rating)
+    suspend fun deleteRating(ids: List<Int>)
 }
