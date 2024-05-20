91b1b450b27d4251469716456ead6682d2f124d8
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-19 17:11:16 -0700

    Slight refactoring and add the ability to add the book to reading list

--- feature/book_detail/src/main/java/com/mmorikawa/book_recommender/feature/book_detail/BookDetailScreen.kt
+++ feature/book_detail/src/main/java/com/mmorikawa/book_recommender/feature/book_detail/BookDetailScreen.kt
@@ -11,13 +11,23 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -29,90 +39,67 @@ import com.mmorikawa.core.designsystem.icon.BookRecIcons
 import com.mmorikawa.core.model.DetailedBook
 import com.mmorikawa.core.ui.UiState
 import com.mmorikawa.core.ui.UiStateWrapper
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun BookDetailRoute(
-    onBackClick: () -> Unit,
-    viewModel: BookDetailViewModel = hiltViewModel()
+    onBackClick: () -> Unit, viewModel: BookDetailViewModel = hiltViewModel()
 ) {
     val bookDetailUiState by viewModel.bookDetailUiState.collectAsStateWithLifecycle()
-    BookDetailScreen(onBackClick, bookDetailUiState)
+    BookDetailScreen(
+        state = bookDetailUiState,
+        onAddToReadingList = { viewModel.addBookToReadingList() },
+        onUndo = { viewModel.removeBookFromReadingList() },
+        onBackClick = onBackClick,
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun BookDetailScreen(
+    state: UiState<DetailedBook>,
+    onAddToReadingList: () -> Unit,
     onBackClick: () -> Unit,
-    bookDetailUiState: UiState<DetailedBook>
+    onUndo: () -> Unit
 ) {
-    UiStateWrapper(uiState = bookDetailUiState) { book ->
-        Scaffold(
-            topBar = {
-                CenterAlignedTopAppBar(navigationIcon = {
-                    IconButton(
-                        onClick = { onBackClick() },
-                    ) {
-                        Icon(
-                            imageVector = BookRecIcons.Back,
-                            contentDescription = "Back arrow",
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    UiStateWrapper(uiState = state) { book ->
+        Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }, topBar = {
+            CenterAlignedTopAppBar(navigationIcon = {
+                IconButton(
+                    onClick = { onBackClick() },
+                ) {
+                    Icon(
+                        imageVector = BookRecIcons.Back,
+                        contentDescription = "Back arrow",
+                    )
+                }
+            }, title = { Text("Book Details") }, actions = {
+                BookDetailDropDownButton(readingListOnclick = {
+                    scope.launch {
+                        val result = snackbarHostState.showSnackbar(
+                            message = "Book added to reading list", actionLabel = "Undo"
                         )
-                    }
-                }, title = { Text("Book Details") },
-                    actions = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = BookRecIcons.AddCircle,
-                                contentDescription = "Book Context Actions"
-                            )
+                        when (result) {
+                            SnackbarResult.ActionPerformed -> {
+                                onUndo()
+                            }
+
+                            SnackbarResult.Dismissed -> {}
                         }
-                    })
-            },
+                    }
+                    onAddToReadingList()
+                }, ratingOnClick = { /*TODO*/ })
+            })
+        },
+
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
             Box(modifier = Modifier.padding(it)) {
-                Column(
-                    modifier = Modifier.verticalScroll(rememberScrollState()),
-                    verticalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Row {
-                        Column {
-                            AsyncImage(
-                                model = book.coverUrl,
-                                contentDescription = "${book.title} cover image.",
-                                contentScale = ContentScale.FillBounds,
-                                modifier = Modifier
-                                    .height(250.dp)
-                                    .width(150.dp)
-                                    .padding(16.dp)
-                            )
-                            Text(
-                                "isbn: ${if (book.isbn != "") book.isbn else "N/A"}",
-                                fontSize = 12.sp
-                            )
-                            Text(
-                                "isbn13: ${if (book.isbn13 != "") book.isbn13 else "N/A"}",
-                                fontSize = 12.sp
-                            )
-                        }
-                        Column(modifier = Modifier.padding(16.dp)) {
-                            Text(
-                                book.title,
-                                fontSize = 24.sp,
-                                modifier = Modifier.padding(vertical = 16.dp)
-                            )
-                            PlainTextStringList(strings = book.authors, prefix = "Authors: ")
-                            PlainTextStringList(strings = book.genres, prefix = "Genres: ")
-                            Text("Page Count: ${if (book.pages != -1) book.pages else "N/A"}")
-                        }
-                    }
-                    Row {
-
-                    }
-                    Text("Book Description:", fontSize = 16.sp)
-                    Text(book.description, fontSize = 12.sp)
-                }
+                BookDetailContentBody(book = book)
             }
         }
     }
@@ -122,4 +109,60 @@ internal fun BookDetailScreen(
 fun PlainTextStringList(strings: List<String>, prefix: String = "") {
     val displayString = strings.toString().removeSurrounding(prefix = "[", suffix = "]")
     Text(text = "$prefix$displayString")
+}
+
+@Composable
+fun BookDetailDropDownButton(readingListOnclick: () -> Unit, ratingOnClick: () -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+    Box {
+        IconButton(onClick = { expanded = true }) {
+            Icon(
+                imageVector = BookRecIcons.AddCircle, contentDescription = "Book Context Actions"
+            )
+        }
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            DropdownMenuItem(text = { Text("Add to reading list") },
+                onClick = { readingListOnclick() })
+            HorizontalDivider()
+            DropdownMenuItem(text = { Text("Add to ratings") }, onClick = { ratingOnClick() })
+        }
+    }
+}
+
+@Composable
+fun BookDetailContentBody(book: DetailedBook) {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row {
+            Column {
+                AsyncImage(
+                    model = book.coverUrl,
+                    contentDescription = "${book.title} cover image.",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier
+                        .height(250.dp)
+                        .width(150.dp)
+                        .padding(16.dp)
+                )
+                Text(
+                    "isbn: ${if (book.isbn != "") book.isbn else "N/A"}", fontSize = 12.sp
+                )
+                Text(
+                    "isbn13: ${if (book.isbn13 != "") book.isbn13 else "N/A"}", fontSize = 12.sp
+                )
+            }
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    book.title, fontSize = 24.sp, modifier = Modifier.padding(vertical = 16.dp)
+                )
+                PlainTextStringList(strings = book.authors, prefix = "Authors: ")
+                PlainTextStringList(strings = book.genres, prefix = "Genres: ")
+                Text("Page Count: ${if (book.pages != -1) book.pages else "N/A"}")
+            }
+        }
+        Text("Book Description:", fontSize = 16.sp)
+        Text(book.description, fontSize = 12.sp)
+    }
 }
