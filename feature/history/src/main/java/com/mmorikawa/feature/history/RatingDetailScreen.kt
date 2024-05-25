314d0cdf5084e8b2fdc01b0f7cdd4983954daf00
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-25 13:05:53 -0700

    Update RatingDetailScreen to contain all components to function

--- feature/history/src/main/java/com/mmorikawa/feature/history/RatingDetailScreen.kt
+++ feature/history/src/main/java/com/mmorikawa/feature/history/RatingDetailScreen.kt
@@ -1,20 +1,35 @@
 package com.mmorikawa.feature.history
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.mmorikawa.core.designsystem.icon.BookRecIcons
+import coil.compose.AsyncImage
 import com.mmorikawa.core.model.Rating
+import com.mmorikawa.core.model.SimpleBook
 import com.mmorikawa.core.ui.UiState
 import com.mmorikawa.core.ui.UiStateWrapper
 
@@ -32,21 +47,94 @@ internal fun RatingDetailRoute(
 internal fun RatingDetailScreen(
     ratingUiState: UiState<Rating>, onRatingTextUpdate: (String) -> Unit
 ) {
-    Scaffold(topBar = { TopAppBar(title = { Text("Book Rating") }) }, floatingActionButton = {
-        FloatingActionButton(onClick = { /*TODO*/ }) {
-            Icon(BookRecIcons.SaveCheckmark, contentDescription = "Save rating button")
-        }
-    }) { padding ->
+    var sliderPosition by remember {
+        mutableFloatStateOf(0f)
+    }
+    val focusManager = LocalFocusManager.current
+    Scaffold(
+        topBar = { CenterAlignedTopAppBar(title = { Text("Book Rating") }) },
+    ) { padding ->
         UiStateWrapper(uiState = ratingUiState) { rating ->
-            Column(modifier = Modifier.padding(padding)) {
-                Row {
-                    TextField(value = rating.ratingText,
-                        onValueChange = { onRatingTextUpdate(it) },
-                        label = { Text("Review") },
-                        placeholder = { Text("Enter review here") })
+            Column(
+                modifier = Modifier.padding(padding), verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                BookHeader(book = rating.book)
+
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Slider(
+                        value = sliderPosition,
+                        onValueChange = { sliderPosition = it },
+                        steps = 49,
+                        valueRange = 0f..5f
+                    )
+                    Text("Rating: %.1f".format(sliderPosition))
+
+                }
+
+                ReviewTextField(text = rating.ratingText, onValueChange = onRatingTextUpdate)
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    OutlinedButton(modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp),
+                        onClick = { /*TODO*/ }) {
+                        Text("Cancel")
+                    }
+                    Button(modifier = Modifier
+                        .weight(1f)
+                        .padding(8.dp), onClick = { /*TODO*/ }) {
+                        Text("Save")
+                    }
                 }
             }
         }
     }
-
 }
+
+@Composable
+fun ReviewTextField(text: String, onValueChange: (String) -> Unit) {
+    TextField(
+        modifier = Modifier
+            .fillMaxHeight(fraction = 0.75f)
+            .fillMaxWidth()
+            .padding(16.dp),
+        value = text,
+        onValueChange = { onValueChange(it) },
+        label = { Text("Review") },
+        placeholder = { Text("Enter review here") },
+    )
+}
+
+@Composable
+fun BookHeader(book: SimpleBook) {
+    Row {
+        AsyncImage(
+            model = book.coverUrl,
+            contentDescription = "${book.title} cover image.",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .height(150.dp)
+                .width(100.dp)
+                .padding(16.dp)
+        )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                book.title, fontSize = 24.sp, modifier = Modifier.padding(vertical = 16.dp)
+            )
+            Text(
+                "Author(s): ${
+                    book.authors.toString().removeSurrounding("[]")
+                }"
+            )
+            Text(
+                "Genre(s): ${
+                    book.genres.toString().removeSurrounding("[]")
+                }"
+            )
+        }
+    }
+}
+
