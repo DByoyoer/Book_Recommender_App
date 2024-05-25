8d88a17eafe58b9967b4ee7d7d8f33e88d96a803
Michael Morikawa <michael.f.morikawa@gmail.com>
2024-05-25 15:19:33 -0700

    Implement saving of a rating in RatingDetailViewModel and move changing dateUpdated to save function

--- feature/history/src/main/java/com/mmorikawa/feature/history/RatingDetailViewModel.kt
+++ feature/history/src/main/java/com/mmorikawa/feature/history/RatingDetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.mmorikawa.feature.history
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -24,6 +25,8 @@ class RatingDetailViewModel @Inject constructor(
 ) : ViewModel() {
     private val ratingDetailArgs: RatingDetailArgs = RatingDetailArgs(savedStateHandle)
     private val bookId = ratingDetailArgs.bookId
+    var isNewRating by mutableStateOf(true)
+        private set
     var uiState by mutableStateOf<UiState<Rating>>(UiState.Loading)
         private set
 
@@ -31,6 +34,7 @@ class RatingDetailViewModel @Inject constructor(
         viewModelScope.launch {
             ratingRepository.getRating(bookId).let { rating ->
                 uiState = if (rating != null) {
+                    isNewRating = false
                     UiState.Success(rating)
                 } else {
                     UiState.Success(
@@ -55,7 +59,7 @@ class RatingDetailViewModel @Inject constructor(
                 book = rating.book,
                 score = rating.score,
                 ratingText = ratingText,
-                dateUpdated = Clock.System.now(),
+                dateUpdated = rating.dateUpdated,
                 dateCreated = rating.dateCreated
             )
         )
@@ -73,6 +77,27 @@ class RatingDetailViewModel @Inject constructor(
                 dateCreated = rating.dateCreated
             )
         )
+    }
+
+    fun saveRating() {
+        var rating = (uiState as UiState.Success).data
+        rating = Rating(
+            book = rating.book,
+            score = rating.score,
+            ratingText = rating.ratingText,
+            dateUpdated = Clock.System.now(),
+            dateCreated = rating.dateCreated
+        )
+        Log.d("Rating Detail", "New($isNewRating)Saved rating: $rating")
+        viewModelScope.launch {
+            if (isNewRating) {
+                ratingRepository.createRating(rating)
+                isNewRating = false
+            } else {
+                ratingRepository.updateRating(rating)
+            }
+        }
+
     }
 
 
